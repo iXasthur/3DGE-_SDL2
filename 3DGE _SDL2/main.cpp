9b8a1f4d5cb37074e9f0ea7fed14d6cf6c31f673
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2019-10-19 21:34:28 +0300

    Initial Commit

---
+++ 3DGE _SDL2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  3DGE _SDL2
+//
+//  Created by Михаил Ковалевский on 19.10.2019.
+//  Copyright © 2019 Mikhail Kavaleuski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
